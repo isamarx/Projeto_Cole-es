a137d546dec832aa680c7dcc4fee3a05f786cd97
Samara Pereira <samarapds26@gmail.com>
2022-07-22 12:13:19 -0300

    Clist

---
+++ Clist/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Clist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lista de animais");
+
+            List<string> lista = new List<string>();
+
+            lista.Add("Macaco");
+            lista.Add("Tigre");
+            lista.Add("Leão");
+            lista.Add("Girafa");
+
+            lista.Insert(1, "Elefante");
+
+            lista.Remove("Girafa");
+
+            lista.RemoveAt(2);
+
+            foreach (string nome in lista)
+            {
+                Console.WriteLine($"Aqui esta a Animal: {nome}");
+            }
+        }
+    }
+}
